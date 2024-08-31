2268eab13538f161073baab4af34774ba41a221e
Jean-Lou Gilbertas <113203030+codi639@users.noreply.github.com>
2024-08-31 12:20:18 +0200

    update args

--- args_module.py
+++ args_module.py
@@ -1,4 +1,3 @@
-# args_module.py
 import argparse
 import os
 import getpass
@@ -11,17 +10,15 @@ def parse_args():
     parser.add_argument('-H', '--host', type=str, default='172.16.20.42', help='InfluxDB host IP address')
     parser.add_argument('-P', '--port', type=int, default=8086, help='InfluxDB port number (default 8086)')
     parser.add_argument('-u', '--username', type=str, default=os.getenv('INFLUXDB_USER', 'user'), help='InfluxDB username')
-    parser.add_argument('-p', '--password', type=str, default=os.getenv('INFLUXDB_PASSWORD', 'password'), help='InfluxDB password')
+    parser.add_argument('-p', '--password', type=str, nargs='?', default=None, help='InfluxDB password (will prompt if not provided)')
     parser.add_argument('-d', '--database', type=str, default='supervision', help='InfluxDB database name')
-    parser.add_argument('-K', '--ask-pass', action='store_true', help='Prompt for the InfluxDB password')
     parser.add_argument('--more-info', action='store_true', help='Display additional information about measurements')
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Prompt for password if --ask-pass is specified
-    if args.ask_pass:
+    # Prompt for password if --password is specified without a value
+    if args.password is None:
         args.password = getpass.getpass(prompt='InfluxDB Password: ')
 
     return args
-
